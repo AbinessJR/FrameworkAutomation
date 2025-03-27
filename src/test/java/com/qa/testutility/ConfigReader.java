f9c3f30506563d1d25cde4eb8345fc4ec7da5c6c
AbinessJR <iamabiness@gmail.com>
2025-03-27 22:15:18 +0530

    First Merge

--- src/test/java/com/qa/testutility/ConfigReader.java
+++ src/test/java/com/qa/testutility/ConfigReader.java
@@ -1,5 +1,22 @@
 package com.qa.testutility;
 
-public class ConfigReader {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
-}
+public class ConfigReader {
+	    static Properties prop = new Properties();
+
+	    static {
+	        try {
+	            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+	            prop.load(fis);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    public static String getBaseUrl() {
+	        return prop.getProperty("base.url");
+	    }
+	}
