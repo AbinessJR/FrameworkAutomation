716eae97ab9bf0a725dcd43b5fa5e2140fccc19b
instadmin <instadmin@localhost>
2025-04-09 23:12:26 +0530

    simple changes

--- src/test/java/com/qa/testscripts/loginscript.java
+++ src/test/java/com/qa/testscripts/loginscript.java
@@ -4,6 +4,7 @@ import com.qa.testpages.LoginPage;
 import com.qa.testutility.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class loginscript extends BaseTest {
 
@@ -11,6 +12,13 @@ public class loginscript extends BaseTest {
     public void testValidLogin() {
     	LoginPage loginPage = new com.qa.testpages.LoginPage(driver);
         loginPage.login("standard_user", "secret_sauce");
-        Assert.assertTrue(driver.getCurrentUrl().contains("inventory.html"));
+      //  Assert.assertTrue(driver.getCurrentUrl().contains("inventory.html"));
+         
+        String header = driver.getCurrentUrl(); 
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(header, "inventory.html");
+
+        
+       
     }
 }
